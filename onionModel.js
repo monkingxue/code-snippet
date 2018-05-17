ea93f3f354046cdff1dd02e044aa6c921e3d7441
monkingxue <yingchenxue@qq.com>
2018-05-17 10:20:26 +0800

    [feat] 增加同步洋葱模型 demo

---
+++ onionModel.js
@@ -0,0 +1,71 @@
+function compose (middleware) {
+	if (!Array.isArray(middleware)) throw new TypeError('Middleware stack must be an array!')
+	for (const fn of middleware) {
+		if (typeof fn !== 'function') throw new TypeError('Middleware must be composed of functions!')
+	}
+
+	/**
+	 * @param {Object} context
+	 * @return {Promise}
+	 * @api public
+	 */
+
+	return function (context, next) {
+		// last called middleware #
+		let index = -1
+		return dispatch(0)
+		function dispatch (i) {
+			if (i <= index) throw new Error('next() called multiple times')
+			index = i
+			let fn = middleware[i]
+			if (i === middleware.length) fn = next
+			if (!fn) return;
+			try {
+				return fn(context, function next () {
+					return dispatch(i + 1)
+				})
+			} catch (err) {
+				throw err
+			}
+		}
+	}
+}
+
+class Application {
+  constructor() {
+    this.middleware = []
+  }
+
+  use(fn) {
+    this.middleware.push(fn);
+    return this;
+  }
+
+  run() {
+    const fn = compose(this.middleware)
+    fn(this, () => console.log('done'))
+  }
+}
+
+const app = new Application();
+
+app.use((ctx, next) => {
+  console.log('before 1');
+  next();
+  console.log('after 1')
+})
+
+// app.use((ctx, next) => {
+//   console.log('before 2');
+//   next();
+//   console.log('after 2')
+// })
+
+app.use((ctx, next) => {
+  console.log('before 3');
+  next();
+  console.log('after 3')
+})
+
+app.run()
+
